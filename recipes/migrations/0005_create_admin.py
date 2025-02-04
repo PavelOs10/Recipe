907e694b75343b7d62f7ec8505a750d02d1bc60c
PavelOs10 <osiov25@gmail.com>
2025-02-05 02:02:24 +0300

    admin

--- recipes/migrations/0005_create_admin.py
+++ recipes/migrations/0005_create_admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 def create_admin(apps, schema_editor):
     User.objects.create_superuser(
-        username='Leej',
+        username='Leej2',
         email='admin@example.com',
         password='9250'
     )
