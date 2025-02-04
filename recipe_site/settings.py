fefca47b83651387a012fd643392f8530cc74b03
PavelOs10 <osiov25@gmail.com>
2025-02-04 17:21:16 +0300

    deploy_fix

--- recipe_site/settings.py
+++ recipe_site/settings.py
@@ -83,7 +83,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'recipes/static']
 STATICFILES_STORAGE = STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
